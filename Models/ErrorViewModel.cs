f7b65db4154f66ade6fb736ac6a0b01e28c64314
Kevin <kevin.castro.chavez0@gmail.com>
2022-07-05 13:17:42 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sacramentMeetingPlanner.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
